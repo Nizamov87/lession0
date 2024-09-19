cfa36926361c5e3826af324817f8e45bac5f21b3
Юрий <yraindeez@gmail.com>
2024-09-20 01:56:54 +1000

    урок 1

---
+++ main.py
@@ -0,0 +1,14 @@
+# "1st program"
+print (9**0.5*5)
+
+# "2st program"
+print (9.99>9.98!=(1000!=1000.1) )
+
+# "3st program"
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+
+# "4st program"       # интересное решение нужно понять))))))
+str = '123.456'
+print (int(float(str)*10)%10)
